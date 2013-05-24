eef23d275f9b33622a3c7a358d245055fb0f280c
keithhedger <keithhedger@396d4681-de98-48fc-96ef-65070ed6547d>
2013-05-24 13:58:39 +0000

    git-svn-id: file:///media/LinuxData/Development/SVN/GtkSu@3 396d4681-de98-48fc-96ef-65070ed6547d

--- GtkSu/src/main.cpp
+++ GtkSu/src/main.cpp
@@ -21,6 +21,8 @@ void shutdown(GtkWidget* widget,gpointer data)
 void doButton(GtkWidget* widget,gpointer data)
 {
 	printf("button %i\n",(int)(bool)data);
+	if((bool)data==false)
+		shutdown(NULL,NULL);
 }
 
 int main(int argc,char **argv)
@@ -43,17 +45,17 @@ int main(int argc,char **argv)
 	gtk_box_pack_start(GTK_BOX(vbox),gtk_label_new("Password"),false,true,0);
 	gtk_box_pack_start(GTK_BOX(vbox),passEntry,false,true,0);
 
-	hbox=gtk_hbox_new(true,0);
+	hbox=gtk_hbox_new(true,4);
 
 	button=gtk_button_new_from_stock(GTK_STOCK_APPLY);
-	gtk_box_pack_start(GTK_BOX(hbox),button,false,true,0);
+	gtk_box_pack_start(GTK_BOX(hbox),button,true,true,0);
 	g_signal_connect(G_OBJECT(button),"clicked",G_CALLBACK(doButton),(void*)true);
 
 	button=gtk_button_new_from_stock(GTK_STOCK_CANCEL);
-	gtk_box_pack_start(GTK_BOX(hbox),button,false,true,0);
+	gtk_box_pack_start(GTK_BOX(hbox),button,true,true,0);
 	g_signal_connect(G_OBJECT(button),"clicked",G_CALLBACK(doButton),(void*)false);
 
-	gtk_box_pack_start(GTK_BOX(vbox),hbox,false,true,0);
+	gtk_box_pack_start(GTK_BOX(vbox),hbox,true,true,0);
 	gtk_container_add(GTK_CONTAINER(window),vbox);
 	gtk_widget_show_all(window);
 	gtk_main();
