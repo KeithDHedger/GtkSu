c2c828f673fb32280b6ee401b96762e6d0f3e3a3
K D Hedger <kdhedger68713@gmail.com>
2018-07-31 14:16:19 +0100

    Updated addresses

--- GtkSu/MakeSuWrap/suwrap.cpp
+++ GtkSu/MakeSuWrap/suwrap.cpp
@@ -1,6 +1,6 @@
 /*
  *
- * ©K. D. Hedger. Sun 27 Sep 20:11:17 BST 2015 kdhedger68713@gmail.com
+ * ©K. D. Hedger. Sun 27 Sep 20:11:17 BST 2015 keithdhedger@gmail.com
 
  * This file (suwrap.cpp) is part of GtkSu.
 
--- GtkSu/src/main.cpp
+++ GtkSu/src/main.cpp
@@ -1,6 +1,6 @@
 /*
  *
- * ©K. D. Hedger. Sun 27 Sep 20:10:42 BST 2015 kdhedger68713@gmail.com
+ * ©K. D. Hedger. Sun 27 Sep 20:10:42 BST 2015 keithdhedger@gmail.com
 
  * This file (main.cpp) is part of GtkSu.
 
